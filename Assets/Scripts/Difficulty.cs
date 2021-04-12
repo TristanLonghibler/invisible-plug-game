50f32f5130c137bc40c4ea016936b6300e2783db
rmhayes97 <rmhnwq@umsystem.edu>
2021-04-12 14:24:44 -0500

    Update Difficulty.cs
    Difficult

--- Assets/Scripts/Difficulty.cs
+++ Assets/Scripts/Difficulty.cs
@@ -1,7 +1,7 @@
 ﻿//As of now, this is not fully implemented. This was a basic implementation of difficulty settings.
 static public class Difficulty {
     public enum Difficulties {Easy, Medium, Hard}
-
+    //Difficult
     //Changing the below variable to Difficulties.Easy or Difficulties.Hard changes the difficulty.
     static public Difficulties currentDifficulty = Difficulties.Medium; //Defaults to Medium
 }
